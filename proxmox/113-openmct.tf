d7dce7a23710646fabc3b8f17077538e02b36d02
Uwe Kamper <me@uwekamper.de>
2026-02-26 22:50:04 +0100

    add VM for openmct (#27)

---
+++ proxmox/113-openmct.tf
@@ -0,0 +1,27 @@
+module "openmct_vm" {
+  source = "../modules/vm"
+  node   = var.node
+  pool   = var.pool
+
+  name  = "openmct"
+  vm_id = "113"
+
+  clone = data.proxmox_virtual_environment_vm.debian_cloud_vm_template.vm_id
+
+  cores  = 2
+  memory = 2048
+
+  disks = [{
+    size_gb = 48
+    storage = var.storage.disk
+  }]
+
+  network = {
+    bridge          = var.network.bridge
+    internal_bridge = proxmox_virtual_environment_network_linux_bridge.internal_bridge.name
+  }
+
+  admins = [
+    local.users["uk"]
+  ]
+}
