5fff1efe11a0e99fb4993f51c4ba988722c684a2
lazee <lazeee@gmail.com>
2026-06-08 17:40:20 +0200

    add lazee matrix bot vm (#28)
    <3
    
    Co-authored-by: Henri <h.schmidt@heinlein-support.de>

---
+++ proxmox/114-lazee.tf
@@ -0,0 +1,27 @@
+module "lazee_vm" {
+  source = "../modules/vm"
+  node   = var.node
+  pool   = var.pool
+
+  name  = "lazee"
+  vm_id = "114"
+
+  clone = data.proxmox_virtual_environment_vm.debian_cloud_vm_template.vm_id
+
+  cores  = 4
+  memory = 4096
+
+  disks = [{
+    size_gb = 64
+    storage = var.storage.disk
+  }]
+
+  network = {
+    bridge          = var.network.bridge
+    internal_bridge = proxmox_virtual_environment_network_linux_bridge.internal_bridge.name
+  }
+
+  admins = [
+    local.users["lazee"]
+  ]
+}
